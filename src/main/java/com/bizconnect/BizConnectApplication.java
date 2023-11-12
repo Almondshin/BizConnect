76d5422e3476a8a99a1aa20995f78b1e1bb8fa50
Almondshin <almondshin8@gmail.com>
2023-11-12 18:04:38 +0900

    DDD BizConnect Project

---
+++ src/main/java/com/bizconnect/BizConnectApplication.java
@@ -0,0 +1,14 @@
+package com.bizconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizConnectApplication.class,
+                args);
+    }
+
+}
---
+++ src/test/java/com/bizconnect/BizConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
