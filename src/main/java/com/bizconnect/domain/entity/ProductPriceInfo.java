c0d0d70ba901d9c073e511ca48c9ea1afa767419
almondshin <Almondshin8@gmail.com>
2023-11-22 11:29:37 +0900

    add annotation

--- src/main/java/com/bizconnect/domain/entity/ProductPriceInfo.java
+++ src/main/java/com/bizconnect/domain/entity/ProductPriceInfo.java
@@ -13,11 +13,14 @@ import javax.persistence.Table;
 @Table(name = "PRODUCT_PRICE_INFO")
 public class ProductPriceInfo {
     /*
+        상품코드 (Product Code) : productCode
         상품종류 (Product Type): productType
         요금 (Price): price
         제공건수 (Number of Provided Items): providedItemCount
         건당요금 (Price Per Item): pricePerItem
         건당 초과 요금 (Excess Charge Per Item): excessChargePerItem
+        서비스 시작일 (Service Start Date) serviceStartDate
+        서비스 종료일 (Service End Date) serviceEndDate
      */
     @Id
     private String productCode;
@@ -27,4 +30,5 @@ public class ProductPriceInfo {
     private String pricePerItem;
     private String excessChargePerItemCount;
     private String serviceStartDate;
+    private String serviceEndDate;
 }
